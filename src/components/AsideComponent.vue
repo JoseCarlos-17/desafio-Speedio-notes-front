712e38206390b75a28d102ffb907deec09be146e
JoseCarlos-17 <josecarlosbsilva.17@gmail.com>
2025-04-21 23:57:05 -0300

    trying to render note created date into noteListComponent

--- src/components/AsideComponent.vue
+++ src/components/AsideComponent.vue
@@ -3,19 +3,63 @@
     <h1>Anotações</h1>
     <div style="width: 440px; margin: 0px 0px 0px 20px;">
       <NotesListComponent
-        :notesList="notesList"/>
+        :notesList="notesList"
+        :loadSelectedNoteMethod="toNoteSelectedView"
+        :deleteSelectedNoteMethod="deleteSelectedNote"
+        :noteCreatedDate="noteCreatedDate"
+      />
     </div>
   </div>
 </template>
 
 <script>
+  import { formatNoteCreatedDate } from '@/utils/formatNoteCreatedDate';
   import NotesListComponent from './NotesListComponent.vue';
+
   export default {
     components: {
       NotesListComponent
     },
 
-    props: ['notesList']
+    props: ['notesList'],
+
+    methods: {
+      noteCreatedDate(date) {
+        return formatNoteCreatedDate(date)
+      },
+
+      loadSelectedNote(note_id) {
+        this.$store.dispatch('showSelectedNote', note_id)
+      },
+
+      toNoteSelectedView(note_id) {
+        this.$route.fullPath === '/noteslist' ?
+        this.$router.push(`/selectednote/${note_id}`) : this.loadSelectedNote(note_id)
+      },
+
+      deleteSelectedNote(note_id) {
+        this.$confirm('Tem certeza de que deseja excluir esta nota?', {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning',
+          center: true
+        }).then(() => {
+            this.$store.dispatch('deleteSelectedNote', note_id)
+            this.$alert("Nota excluída", {
+            showConfirmButton: false,
+            type: 'success',
+            center: true,
+          })
+          setTimeout(() => {
+            this.$router.go(0)
+          }, 2000);
+        }).catch(error => error);
+      }      
+    },
+
+    mounted() {
+      this.loadSelectedNote()
+    }
   }
 </script>
 
