ddfb26cbb406ac5406e8296b34b7731a69dbe8df
JoseCarlos-17 <josecarlosbsilva.17@gmail.com>
2025-05-16 14:28:13 -0300

    refactoring the notes list view

--- src/views/NotesListView.vue
+++ src/views/NotesListView.vue
@@ -3,7 +3,11 @@
     <h1>Anotações</h1>
     <el-row>
       <el-col :span="8" :offset="8">
-        <NotesListComponent :notesList="notesList"/>
+        <NotesListComponent
+          :notesList="notesList"
+          :loadSelectedNoteMethod="toNoteSelectedView"
+          :deleteSelectedNoteMethod="deleteSelectedNote"
+        />
       </el-col>
     </el-row>
   </div>
@@ -19,6 +23,28 @@
     methods: {
       loadNoteList() {
         return this.$store.dispatch('getNotesList')
+      },
+
+      toNoteSelectedView(note_id) {
+        this.$route.fullPath === '/noteslist' ?
+        this.$router.push(`/selectednote/${note_id}`) : this.loadSelectedNote(note_id)
+      },
+
+      deleteSelectedNote(note_id) {
+        this.$confirm('Tem certeza de que deseja excluir esta nota?', {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'warning',
+          center: true
+        }).then(() => {
+            this.$store.dispatch('deleteSelectedNote', note_id)
+            this.$alert("Nota excluída", {
+            showConfirmButton: false,
+            type: 'success',
+            center: true,
+          })
+          setTimeout(() => this.$router.go(0), 2000);
+        }).catch(error => error);
       }
     },
 
